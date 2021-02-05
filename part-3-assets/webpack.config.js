f1df11601b5f70b9f7bfd14dd375e83717801191
Michael Yap <michaelyap07@gmail.com>
2021-02-05 17:13:28 +0800

    Starting point for part 3

---
+++ part-3-assets/webpack.config.js
@@ -0,0 +1,44 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+
+module.exports = {
+  mode: 'development',
+	entry: {
+		index: './src/pages/index/index.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'postcss-loader'
+        ]
+      },
+    ]
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      chunks: ['index'],
+      inject: false,
+      filename: 'index.html',
+      template: 'src/pages/index/index.html',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    publicPath: '/'
+  },
+}
