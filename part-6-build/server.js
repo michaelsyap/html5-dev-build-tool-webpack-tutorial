614aadd50fad18ac3fc5896905ca2b0b7ae71f1d
Michael Yap <michaelyap07@gmail.com>
2021-03-04 21:54:50 +0800

    Extract webpack configuration for development and build

--- part-6-build/server.js
+++ part-6-build/server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const webpack = require("webpack")
 const webpackDevMiddleware = require("webpack-dev-middleware")
 const app = express()
-const config = require("./webpack.config.js")
+const config = require("./webpack.dev.config.js")
 const compiler = webpack(config)
 
 
--- part-6-build/webpack.config.js
+++ part-6-build/webpack.config.js
@@ -1,11 +1,15 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const pages = require('./pages')
 
 module.exports = {
-  mode: 'development',
 	entry: {
-		index: './src/pages/index/index.js',
-		carousel: './src/pages/carousel/carousel.js'
+    ...pages.reduce((prev, next) => {
+      return {
+        ...prev,
+        [next.entryName]: next.entryScript
+      }
+    }, {})
   },
   module: {
     rules: [
@@ -14,58 +18,13 @@ module.exports = {
         exclude: /(node_modules|bower_components)/,
         use: {
           loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
         }
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          {
-            loader: 'file-loader',
-          }
-        ]
-      },
-      {
-        //This is a regex of file extensions for fonts
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [
-          'file-loader',
-        ],
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'postcss-loader'
-        ]
       },
     ]
   },
-  devtool: 'inline-source-map',
-  plugins: [
-    new HtmlWebpackPlugin({
-      chunks: ['index'],
-      inject: false,
-      filename: 'index.html',
-      template: 'src/pages/index/index.html',
-    }),
-    new HtmlWebpackPlugin({
-      chunks: ['carousel'],
-      inject: false,
-      filename: 'carousel.html',
-      template: 'src/pages/carousel/carousel.html',
-    }),
-  ],
   resolve: {
     alias: {
       Assets: path.resolve(__dirname, "src/assets")
     }
-  },
-  output: {
-    filename: '[name].bundle.js',
-    publicPath: '/'
   },
 }
